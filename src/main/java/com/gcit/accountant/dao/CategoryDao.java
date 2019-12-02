77f017fb991e9e860df769e3a8fae6f1886c9670
cjc2785 <cory.colbert@smoothstack.com>
2019-12-02 16:15:47 -0500

    Removed unused method from category dao

--- src/main/java/com/gcit/accountant/dao/CategoryDao.java
+++ src/main/java/com/gcit/accountant/dao/CategoryDao.java
@@ -2,12 +2,9 @@ package com.gcit.accountant.dao;
 
 import com.gcit.accountant.model.*;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface CategoryDao extends JpaRepository<Category, Integer> {
 
-	public List<Category> findByName(String name);
 }
